f050456f6318664a265d16e6141a180e196691b8
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-24 18:53:21 -0400

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import NavBar from './components/NavBar';
 import './App.css';
+import ProjectGrid from './components/ProjectGrid';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <div style={{ paddingTop: '50px' }}>
+        <ProjectGrid />
+      </div>
     </div>
   );
 }

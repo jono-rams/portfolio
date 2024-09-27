f32d42bafba393b818fd581ec6759b0aa967bd91
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-09-27 09:22:04 -0400

    v2.2 (#19)
    * Added footer and made some style changes
    
    * 2.2 preview
    
    * Fixed Navbar extension bug for mobile screens

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,19 +4,27 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { Link, NavLink, Outlet } from 'react-router-dom';
 
 import './NavBar.css';
+import Footer from './Footer';
+import { useState } from 'react';
 
 const NavBar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavLinkClick = () => {
+    setExpanded(false);
+  }
+
   return (
     <div>
       <header>
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar expanded={expanded} onToggle={(expanded) => setExpanded(expanded)} bg="dark" variant="dark" expand="lg">
           <Container className='text-center'>
             <Navbar.Brand className='me-auto'><NavLink className="nav-brand" to='/'>Jonathan Rampersad's Portfolio</NavLink></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" className='ms-auto' />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
-                <NavLink className='nav-body nav-link' to="/">Home</NavLink>
-                <NavLink className='nav-body nav-link' to="/about">About</NavLink>
+                <NavLink onClick={handleNavLinkClick} className='nav-body nav-link' to="/">Home</NavLink>
+                <NavLink onClick={handleNavLinkClick} className='nav-body nav-link' to="/about">About</NavLink>
                 <Link className='nav-body nav-link' to="https://github.com/jono-rams" target="_blank">
                   <FontAwesomeIcon icon={faGithub} /> GitHub
                 </Link>
@@ -32,6 +40,7 @@ const NavBar = () => {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };

4d3e79109ff1314fc5f2fb07b48a629961b66384
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-09-29 18:28:40 -0400

    Added new recipe site (#22)

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <div className="container about-me">
       <header className="text-center mb-4">
-        <h1>About Me</h1>
+        <h1 className='pb-2'>About Me</h1>
         <h1 className="catch">Problem-Solver. Quick Learner. Passionate Programmer.</h1>
       </header>
 

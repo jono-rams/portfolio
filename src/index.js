ce2f2ebad852ab9676acab05ef8f983eafd427d7
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-10-07 07:41:49 -0400

    Revert "import config at setup"
    This reverts commit 61ba4058f754d999e14da9c574437e20ff31745e.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './firebase/config';
 import './index.css';
 import App from './App';
 

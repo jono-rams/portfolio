56fa7edfc14d07df4bc41afd3db1dd963e83d5b1
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-12 21:12:42 -0400

    Updated About-Me Page and Full code refactor (#29)

--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -1,87 +1,108 @@
-import { useEffect, useState } from 'react';
-import { analytics, firebaseStorage, firestoreDatabase } from '../firebase/config';
-import { collection, onSnapshot } from 'firebase/firestore';
-import { getDownloadURL, ref } from 'firebase/storage';
+import { useEffect, useState } from "react";
+import {
+  analytics,
+  firebaseStorage,
+  firestoreDatabase,
+} from "../firebase/config";
+import { collection, onSnapshot } from "firebase/firestore";
+import { getDownloadURL, ref } from "firebase/storage";
 
-import './ProjectGrid.css';
-import { logEvent } from 'firebase/analytics';
+import "./ProjectGrid.css";
+import { logEvent } from "firebase/analytics";
 
 const techColors = {
-  'React': '#00bbbb',
-  'Node.js': 'green',
-  'Express': 'orange',
-  'MongoDB': 'darkgreen',
-  'Firebase': '#dddd00',
-  'Vanilla JS': 'purple',
-  'Webpack': 'lightblue',
-  'Bootstrap': 'gray',
-  'Babel': 'lightgray'
+  React: "#00bbbb",
+  "Node.js": "green",
+  Express: "orange",
+  MongoDB: "darkgreen",
+  Firebase: "#dddd00",
+  "Vanilla JS": "purple",
+  Webpack: "lightblue",
+  Bootstrap: "gray",
+  Babel: "lightgray",
   // ... add more colors for other technologies
 };
 
 const ProjectGrid = () => {
-
   const [projects, setProjects] = useState(null);
 
   useEffect(() => {
-    onSnapshot(collection(firestoreDatabase, 'sites'), (snapshot) => {
+    onSnapshot(collection(firestoreDatabase, "sites"), (snapshot) => {
       const projs = snapshot.docs.map((doc) => ({
         id: doc.id,
-        ...doc.data()
+        ...doc.data(),
       }));
 
-      Promise.all(projs.map(p => {
-        const iRef = ref(firebaseStorage, p.image);
-        return getDownloadURL(iRef).then((url) => ({
-          ...p,
-          image: url
-        }));
-      })).then(updatedProjs => {
+      Promise.all(
+        projs.map((p) => {
+          const iRef = ref(firebaseStorage, p.image);
+          return getDownloadURL(iRef).then((url) => ({
+            ...p,
+            image: url,
+          }));
+        })
+      ).then((updatedProjs) => {
         setProjects(updatedProjs);
       });
     });
   }, []);
 
   const handleThumbnailClick = (projectName) => {
-    logEvent(analytics, 'select_content', {
-      content_type: 'project_thumbnail',
-      item_id: projectName
+    logEvent(analytics, "select_content", {
+      content_type: "project_thumbnail",
+      item_id: projectName,
     });
   };
 
   return (
     <div className="proj-grid container text-center">
       <header>
-        <h1 className='pb-3'>My Websites</h1>
+        <h1 className="pb-3">My Websites</h1>
       </header>
 
-      {!projects && <div className='text-center'>Loading website details...</div>}
+      {!projects && (
+        <div className="text-center">Loading website details...</div>
+      )}
 
-      <div className="row row-cols-2">
-        {projects && projects.map((project) => (
-          <div className="col mb-4" key={project.title}>
-            <div className="card">
-              <a href={project.link} target="_blank" rel="noopener noreferrer" onClick={() => handleThumbnailClick(project.title)}>
-                <img src={project.image} className="card-img-top" alt={project.title} />
-                <div className="card-body">
-                  <h5 className="card-title">{project.title}</h5>
+      <main>
+        <div className="row row-cols-2">
+          {projects &&
+            projects.map((project) => (
+              <div className="col mb-4" key={project.title}>
+                <div className="card">
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={() => handleThumbnailClick(project.title)}
+                  >
+                    <img
+                      src={project.image}
+                      className="card-img-top"
+                      alt={project.title}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{project.title}</h5>
+                    </div>
+                    <div className="card-footer">
+                      {project.technologies.map((tech) => (
+                        <span
+                          key={tech}
+                          className="badge rounded-pill me-2 fs-6 text-white"
+                          style={{
+                            backgroundColor: techColors[tech] || "gray",
+                          }}
+                        >
+                          {tech}
+                        </span>
+                      ))}
+                    </div>
+                  </a>
                 </div>
-                <div className="card-footer">
-                  {project.technologies.map((tech) => (
-                    <span
-                      key={tech}
-                      className="badge rounded-pill me-2 fs-6 text-white"
-                      style={{ backgroundColor: techColors[tech] || 'gray' }}
-                    >
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-              </a>
-            </div>
-          </div>
-        ))}
-      </div>
+              </div>
+            ))}
+        </div>
+      </main>
     </div>
   );
 };
---
+++ src/pages/about/Projects.js
@@ -0,0 +1,96 @@
+import { Link } from "react-router-dom";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="mb-4 projs">
+      <h2 className="text-center py-3">Project Highlights</h2>
+      <div>
+        <article className="mb-3">
+          <Link to="https://github.com/jono-rams/rt-chatroom" target="_blank">
+            <div className="card">
+              <div className="card-body">
+                <h3 className="card-title">
+                  Realtime Chatroom (JavaScript, Firebase)
+                </h3>
+                <p className="card-text">
+                  Developed a dynamic chat application with multiple channels,
+                  allowing users to communicate in real-time. Implemented
+                  Firebase Firestore for efficient chat storage and retrieval,
+                  ensuring seamless user interactions.
+                </p>
+              </div>
+            </div>
+          </Link>
+        </article>
+
+        <article className="mb-3">
+          <Link
+            to="https://github.com/jono-rams/Taxi-Booking-System"
+            target="_blank"
+          >
+            <div className="card">
+              <div className="card-body">
+                <h3 className="card-title">
+                  Taxi Booking System (Python, SQLite3)
+                </h3>
+                <p className="card-text">
+                  Built a comprehensive taxi booking platform featuring user
+                  roles for customers, drivers, and administrators. Utilized
+                  SQLite3 for data management and implemented user
+                  authentication and booking functionalities.
+                </p>
+              </div>
+            </div>
+          </Link>
+        </article>
+
+        <article className="mb-3">
+          <Link to="https://github.com/jono-rams/weather-js" target="_blank">
+            <div className="card">
+              <div className="card-body">
+                <h3 className="card-title">
+                  Weather Website (JavaScript, Bootstrap, AccuWeather API)
+                </h3>
+                <p className="card-text">
+                  Created a user-friendly website that provides current weather
+                  conditions for any city, styled with Bootstrap and utilizing
+                  AccuWeather's API for accurate and up-to-date data.
+                </p>
+              </div>
+            </div>
+          </Link>
+        </article>
+
+        <article className="mb-3">
+          <Link to="https://github.com/jono-rams/magic-memory" target="_blank">
+            <div className="card">
+              <div className="card-body">
+                <h3 className="card-title">Memory Match Game (React)</h3>
+                <p className="card-text">
+                  Designed an engaging memory match game using React, showcasing
+                  my ability to create interactive and visually appealing web
+                  applications.
+                </p>
+              </div>
+            </div>
+          </Link>
+        </article>
+
+        <article className="mb-3">
+          <Link to="https://github.com/jono-rams/Enigma" target="_blank">
+            <div className="card">
+              <div className="card-body">
+                <h3 className="card-title">Enigma Machine Recreation (C++)</h3>
+                <p className="card-text">
+                  Replicated the functionality of the Enigma machine,
+                  demonstrating proficiency in C++ and a passion for historical
+                  cryptography.
+                </p>
+              </div>
+            </div>
+          </Link>
+        </article>
+      </div>
+    </section>
+  );
+}

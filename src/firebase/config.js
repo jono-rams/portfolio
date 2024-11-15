8102647caab7a294b7325d9604a3b692a7e16ddc
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-15 12:01:31 -0400

    Typo fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import { getStorage } from 'firebase/storage';
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: proccess.env.FIREBASE_API_KEY,
-  authDomain: proccess.env.FIREBASE_AUTH_DOMAIN,
-  projectId: proccess.env.FIREBASE_PROJECT_ID,
-  storageBucket: proccess.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: proccess.env.FIREBASE_MESSAGE_SENDER_ID,
-  appId: proccess.env.FIREBASE_APP_ID,
-  measurementId: proccess.env.FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // init firebase

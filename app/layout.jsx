8642777aa45953ea77cbf700888b4f884023f7b4
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2025-01-10 21:37:11 -0400

    Added Google Analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Script from 'next/script';
+import { GA_TRACKING_ID } from '@/utils/gtag';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,6 +28,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased my-app`}>
+        {/* Google Analytics */}
+        <Script 
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script 
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+
         <NavBar /> 
         {children}
         <Footer />

e0d9e3e934ec568fa8de904bcce7f96f62e37e3b
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2025-01-12 20:23:52 -0400

    Updated Skills Section

--- app/about/Skills.jsx
+++ app/about/Skills.jsx
@@ -10,6 +10,7 @@ import Frameworks from "./skills/Frameworks";
 import Databases from "./skills/Databases";
 import BaaS from "./skills/BaaS";
 import ToolsAndTech from "./skills/ToolsAndTech";
+import Api from "./skills/Api";
 
 export default function Skills() {
   return (
@@ -17,12 +18,13 @@ export default function Skills() {
       <h2 className="pb-3 text-4xl font-bold text-center">Technical Skills</h2>
 
       <Tabs defaultValue="languages">
-      <TabsList className="grid w-full h-full grid-rows-5 md:grid-rows-2 md:grid-cols-3 justify-self-center lg:h-12 lg:grid-rows-1 lg:grid-cols-5">
+      <TabsList className="grid w-full h-full grid-rows-6 lg:grid-rows-2 lg:grid-cols-3 md:grid-rows-2 md:grid-cols-3 justify-self-center xl:h-12 xl:grid-rows-1 xl:grid-cols-6 ">
         <TabsTrigger value="languages">Languages</TabsTrigger>
         <TabsTrigger value="frameworks">Frameworks</TabsTrigger>
         <TabsTrigger value="databases">Databases</TabsTrigger>
         <TabsTrigger value="baas">BaaS (Backend as a Service)</TabsTrigger>
         <TabsTrigger value="tools-and-tech">Tools & Technologies</TabsTrigger>
+        <TabsTrigger value="apis">APIs</TabsTrigger>
       </TabsList>
       <TabsContent value="languages">
         <Languages />
@@ -38,6 +40,9 @@ export default function Skills() {
       </TabsContent>
       <TabsContent value="tools-and-tech">
         <ToolsAndTech />
+      </TabsContent>
+      <TabsContent value="apis">
+        <Api />
       </TabsContent>
     </Tabs>
     </aside>

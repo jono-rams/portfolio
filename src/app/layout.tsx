be758c5ce9c5e7fefe29ac186c8adab6b8a27e45
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2025-06-27 12:53:57 -0400

    Add Vercel Anlaytics and Speed Insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/layout/Navbar';
@@ -24,6 +26,8 @@ export default function RootLayout({
           <main className="flex-grow container mx-auto p-4">{children}</main>
           <Footer />
         </div>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
